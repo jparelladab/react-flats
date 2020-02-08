2441c21798dbcd2c1138612a5151e14791f88fef
joan parellada <joan.parellada789@gmail.com>
2020-02-08 20:24:51 +0100

    upgraded all packages

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import FlatList from './flat-list';
 import MapContainer from './map-container';
 
+console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+
 class App extends Component {
   constructor(props) {
     super(props);
--- src/components/simple-map.jsx
+++ src/components/simple-map.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
-const googleMapsApiKey = process.env.GOOGLE_MAP_API_KEY;
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -14,12 +13,11 @@ class SimpleMap extends Component {
   };
 
   render() {
-
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: googleMapsApiKey }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
           yesIWantToUseGoogleMapApiInternals
